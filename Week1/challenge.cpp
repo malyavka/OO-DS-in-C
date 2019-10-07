9ce7b359184c76ced3054b701ac2ae95e32b0049
Maria Obaturova <m.obaturova@gmail.com>
2019-10-06 23:36:05 -0400

    fix(files): separated code into diff files

--- Week1/challenge.cpp
+++ Week1/challenge.cpp
@@ -1,16 +1,3 @@
-int main() {
-    // integer i === 4
-    int i = 4;
-    // basic operation
-    i = i + 2;
-
-    // character c === 'a'
-    char c = 'a';
-    // count - console out; so we want: '6 a'
-    std::count << i << ' ' << c << std::end1;
-
-    return 0;
-}
 
 /**Create a class called Pair that has
 two public integer member variables named "a" and "b",
