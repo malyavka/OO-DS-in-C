f4195f118c49776daf7d5964a1c5e54e3ba5da92
Maria Obaturova <m.obaturova@gmail.com>
2019-10-09 22:10:25 -0400

    Test passed. Chalennge 2 week

--- Week 2/challenge/main.cpp
+++ Week 2/challenge/main.cpp
@@ -28,3 +28,12 @@
 // Also note, the autograder does not care about letter case, punctuation,
 // or spacing! But it does care about spelling and the order of words!
 
+#include <iostream>
+
+int main() {
+
+    std::cout << "Hello, world" << std::endl;
+    std::cout << "Greetings from Illinois!" << std::endl;
+
+  return 0;
+}
