fe3ee4697466c3f13f0a0e8a89890fc8c6ae4aa4
Maria Obaturova <m.obaturova@gmail.com>
2019-11-05 01:13:13 -0500

    fizzBuzz

---
+++ codecademy/fizzBuzz.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+  for (int i = 1;  i <= 100; i++) {
+    if (i % 3 == 0) {
+      std::cout << "Fizz!\n";
+      continue;
+    }
+    else if (i%5 == 0) {
+      std::cout << "Buzz!\n";
+      continue;
+    }
+    else if (i%3 == 0 && i%5 == 0){
+      std::cout << "FizzBuzz!\n";
+    }
+    else {
+     std::cout << i  << "\n";
+    }
+  }
+}
